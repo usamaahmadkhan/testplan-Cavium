7371fc94d6a873747ea3ea9bd7fb49099e8d20a2
usamaahmadkhan <usama.ahmad.khan@hotmail.com>
2017-07-03 16:03:58 +0500

    Fixed test-ip-dhc.sh

--- plan/test-scripts/test-ip-dhc.sh
+++ plan/test-scripts/test-ip-dhc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Requesting a new IP address from dhcp...
-sudo dhclient -q
-#Checking syslog for last dhcp request...
-#Last line in syslog is:"
-sudo tail --lines=1 /var/log/syslog > /dev/null 2>&1
-
-count=$(sudo tail --lines=1 /var/log/syslog | grep "bound" | wc -l)
-
-if [ $count -eq 1 ]
-then echo "pass"
+sudo dhclient -q > /dev/null 2>&1
+if [ $? -eq 0 ]
+then 
+    count=$(ip addr | grep dynamic | wc -l)
+    echo $count
+    if [ $count -eq 1 ]
+    then echo "pass"
+    else echo "fail"
+    fi 
 else echo "fail"
 fi
